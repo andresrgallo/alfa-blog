fd9df25d2203c40a10dcf5fc97be5c2e3cbb7274
andresrgallo <andresgallo@hotmail.com>
2018-08-02 16:10:18 +1000

    Add bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: { case_sensitive: false}, length: { minimum: 3, maximum: 20}
     email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, uniqueness: { case_sensitive: false}, format: { with: email_regex}, length: { maximum: 50}
+    has_secure_password
 end
