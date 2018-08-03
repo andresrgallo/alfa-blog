e141c0791f51f59318b657b362d339da5f2f61c2
andresrgallo <andresgallo@hotmail.com>
2018-08-03 10:23:08 +1000

    Add authentication to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :update, :edit]
+
+    def index
+        @users = User.all
+    end
 
     def new
         @user = User.new
@@ -7,7 +12,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:success] = "Welcome to Alpha-Blog"
+            flash[:success] = "Welcome to Alpha-Blog #{@user.username}"
             redirect_to articles_path
         else
             render 'new'
@@ -15,11 +20,9 @@ class UsersController < ApplicationController
     end
     
     def edit
-        @user = User.find(params[:id])
     end
     
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:success] = "Your details have been successfully updated"
             redirect_to articles_path
@@ -29,10 +32,13 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
     end
 
     private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
