8fe0869bc4f00b03aad204b1df35aa1318a46a33
andresrgallo <andresgallo@hotmail.com>
2018-08-03 14:52:07 +1000

    Add restrictions to articles and users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :update, :edit]
+    before_action :require_same_user, only: [:edit, :update]
 
     def index
         @users = User.all
@@ -12,8 +13,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to Alpha-Blog #{@user.username}"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'new'
         end
@@ -42,5 +44,12 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def require_same_user
+        if  current_user != @user
+            flash[:danger] = "You can only update your profile"
+            redirect_to root_path
+        end
     end
 end
